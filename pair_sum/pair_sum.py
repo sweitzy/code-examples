fcc4a8ef10142669669bb754d3eaf8a8225c5514
Scott Weitzenkamp <scott_weitzenkamp@yahoo.com>
2017-11-03 09:56:24 -0700

    slower version but uses less memory hopefully

--- pair_sum/pair_sum.py
+++ pair_sum/pair_sum.py
@@ -28,6 +28,28 @@ def has_pair_with_sum(data, sum):
     return False
 
 
+def has_pair_with_sum_slow(data, sum):
+    """Return True if two numbers in data add up to sum
+
+    BIG O: O(n * n)
+
+    >>> print(has_pair_with_sum_slow([1, 2, 3, 4], 8))
+    False
+    >>> print(has_pair_with_sum_slow([1, 2, 4, 4], 8))
+    True
+    """
+
+    # This version is slow, but does use less memory
+    # due to no dictionary.
+
+    # TODO: more Pythonic way to do this?
+    for i in range(len(data)):
+        for j in range(i + 1, len(data)):
+            if data[i] + data[j] == sum:
+                return True
+    return False
+
+
 # validate documentation tests
 if __name__ == '__main__':
 #    print(has_pair_with_sum([1, 2, 3, 4], 8))
